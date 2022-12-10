c7ea8cb5f31320c28c0f561c5db530b1b447a8a7
lyazj <seeson@pku.edu.cn>
2022-12-10 20:27:12 +0800

    Revise rand.

--- rand/rand.c
+++ rand/rand.c
@@ -7,25 +7,30 @@
 #define N 10000
 #define R 10000
 
-int main(void)
+int main(int argc, const char *argv[])
 {
   time_t t;
   long seed;
   int i, r;
   double d;
   unsigned long dd;
-  unsigned long b[10];
+  unsigned long b[10] = { };
 
-  if((t = time(0)) == -1)
-    err(EXIT_FAILURE, "time");
-  seed = t ^ getpid();
+  if(argc == 1)
+  {
+    if((t = time(0)) == -1)
+      err(EXIT_FAILURE, "time");
+    seed = t ^ getpid();
+  }
+  else if(argc == 2)
+    seed = atoi(argv[1]);
+  else
+    errx(EXIT_FAILURE, "expect 0 or 1 arguments");
   srand48(seed);
   fprintf(stderr, "seed: %ld\n", seed);
 
   for(r = 0; r < R; ++r)
   {
-    for(i = 0; i < 10; ++i)
-      b[i] = 0;
     for(i = 0; i < N; ++i)
     {
       d = drand48();  // uniformly over [0.0, 1.0)
@@ -33,7 +38,12 @@ int main(void)
         errx(EXIT_FAILURE, "unexpected value: %a", d);
       ++b[dd];
     }
-    printf("%lu\n", b[3]);
+    for(i = 0; i < 10; ++i)
+    {
+      printf("%6lu", b[i]);
+      b[i] = 0;
+    }
+    printf("\n");
   }
 
   return 0;
